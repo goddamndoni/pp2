33331ad69e06f4b59c3086c7e1cd5459cdde6f39
Danabek <raimkhanovd@gmail.com>
2023-01-26 11:34:09 +0600

    update

--- 1.py
+++ 1.py
@@ -1,2 +1,10 @@
-print("HelloWorld")
-#git
+from math import prod
+from random import randrange
+
+a = int(input())
+d = int(input())
+l = [0]*a
+for i in range(a):
+    l[i] = a*pow(d,i)
+print(l)
+
--- a.py
+++ a.py
@@ -1,1 +1,6 @@
-ksdkflghdjk ;h;lkPring;lkjldh g 
+x = []
+k = 0 
+while x!=0:
+    x.append()
+    k+=1
+print(x)
