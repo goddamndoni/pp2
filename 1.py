16483006b92d17c7c66c80b2729c65457b9c1184
Danabek <raimkhanovd@gmail.com>
2023-01-27 20:47:42 +0600

    new folder

--- 1.py
+++
@@ -1,7 +0,0 @@
-from random import randrange
-
-
-n = int(input())
-l = [0]*n
-for i in range(n):
-    l[i] = randrange(10)
--- a.py
+++
@@ -1,6 +0,0 @@
-x = []
-k = 0 
-while x!=0:
-    x.append()
-    k+=1
-print(x)
